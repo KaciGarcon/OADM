20eb3e5b4c7deda328d41514face33d9ed9fc7dd
herhor67 <herhor67@interia.pl>
2019-11-29 19:39:47 +0100

    Update Steepest.m

--- multivariable/Steepest.m
+++ multivariable/Steepest.m
@@ -12,12 +12,12 @@ x0 = [0; 0];
 while true
     x0
     
-    d1 = gf(x0)
+    d1 = -gf(x0)
     
     avect = sort([(xMin-x0(1))/d1(1), (yMin-x0(2))/d1(2), (xMax-x0(1))/d1(1), (yMax-x0(2))/d1(2)])
-    alpha = fminbnd(@(a) f(x0-a*d1), avect(2), avect(3))
+    alpha = fminbnd(@(a) f(x0+a*d1), avect(2), avect(3))
     
-    x1 = x0-alpha*d1
+    x1 = x0+alpha*d1
     x0 = x1;
     
     if (sqrt(sum(abs(alpha*d1).^2)) < 0.001)
