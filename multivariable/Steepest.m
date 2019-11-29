5bba85509bb7eaadf2a68bbb463103d9778eee3c
herhor67 <herhor67@interia.pl>
2019-11-29 19:23:02 +0100

    Update Steepest.m

--- multivariable/Steepest.m
+++ multivariable/Steepest.m
@@ -8,28 +8,19 @@ xMin = -1; xMax = 1;
 yMin = -1; yMax = 1;
 
 x0 = [0; 0];
-d1 = -gf(x0);
-avect = sort([(xMin-x0(1))/d1(1), (yMin-x0(2))/d1(2), (xMax-x0(1))/d1(1), (yMax-x0(2))/d1(2)])
-alpha = fminbnd(@(a) f(x0+a*d1), avect(2), avect(3))
-x1 = x0+alpha*d1;
 
 while true
     x0
-    x1
     
-    grad0 = gf(x0)
-    grad1 = gf(x1)
+    d1 = gf(x0)
     
-    d2 = -grad1
+    avect = sort([(xMin-x0(1))/d1(1), (yMin-x0(2))/d1(2), (xMax-x0(1))/d1(1), (yMax-x0(2))/d1(2)])
+    alpha = fminbnd(@(a) f(x0-a*d1), avect(2), avect(3))
     
-    avect = sort([(xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2), (xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2)])
-    alpha = fminbnd(@(a) f(x1+a*d2), avect(2), avect(3))
-    
-    x2 = x1+alpha*d2
+    x1 = x0-alpha*d1
     x0 = x1;
-    x1 = x2;
     
-    if (sqrt(sum(abs(alpha*d2).^2)) < 0.001)
+    if (sqrt(sum(abs(alpha*d1).^2)) < 0.001)
         break
     end
 end
