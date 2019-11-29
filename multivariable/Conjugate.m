07ff009d4c0908a6e3732e98deff4d1518a6d5f8
herhor67 <herhor67@interia.pl>
2019-11-29 19:28:16 +0100

    Update Conjugate.m

--- multivariable/Conjugate.m
+++ multivariable/Conjugate.m
@@ -8,11 +8,13 @@ xMin = -1; xMax = 1;
 yMin = -1; yMax = 1;
 
 x0 = [0; 0];
+
 d1 = -gf(x0);
 avect = sort([(xMin-x0(1))/d1(1), (yMin-x0(2))/d1(2), (xMax-x0(1))/d1(1), (yMax-x0(2))/d1(2)])
 alpha = fminbnd(@(a) f(x0+a*d1), avect(2), avect(3))
 x1 = x0+alpha*d1;
 
+iterations=0;
 while true
     x0
     x1
@@ -24,22 +26,26 @@ while true
     
     d2 = B1*d1 - grad1
     
-%    avect = sort([(xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2), (xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2)])
-%    alpha = fminbnd(@(a) f(x1+a*d2), avect(2), avect(3))      % tit for tat
+    avect = sort([(xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2), (xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2)]);
+    alpha = fminbnd(@(a) f(x1+a*d2), avect(2), avect(3))      % tit for tat
 
-    aMin = max((xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2))
-    aMax = min((xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2))
-    if aMin <= aMax
-        alpha = fminbnd(@(a) f(x1+a*d2), aMin, aMax)
-    else
-        alpha = 0
-    end
+%    aMin = max((xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2))       % tit for tat
+%    aMax = min((xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2))
+%    if aMin <= aMax
+%        alpha = fminbnd(@(a) f(x1+a*d2), aMin, aMax)
+%    else
+%        alpha = 0
+%    end
     
     x2 = x1+alpha*d2
     x0 = x1;
     x1 = x2;
     
+    iterations = iterations+1;
+    
     if (sqrt(sum(abs(alpha*d2).^2)) < 0.001)
         break
     end
 end
+
+iterations
